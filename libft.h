e4a6f796f05a4b030f06280b036a4ffe5db69b96
Sergiu BOCANCIA <sbocanci@e3r8p6.clusters.42paris.fr>
2022-11-21 18:07:59 +0100

    modified to fit norm requirements

--- libft.h
+++ libft.h
@@ -6,15 +6,15 @@
 /*   By: sbocanci <sbocanci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 15:32:22 by sbocanci          #+#    #+#             */
-/*   Updated: 2022/11/21 12:53:31 by sbocanci         ###   ########.fr       */
+/*   Updated: 2022/11/21 18:00:26 by sbocanci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stddef.h>
-#include <stdlib.h>
+# include <stddef.h>
+# include <stdlib.h>
 
 int		ft_atoi(char *str);
 void	ft_bzero(void *s, size_t n);
@@ -33,15 +33,15 @@ int		*ft_memset(void *s, int c, size_t n);
 char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t  ft_strlen(const char *s);
-char    *ft_strtrim(char const *s1, char const *set);
+size_t	ft_strlen(const char *s);
+char	*ft_strtrim(char const *s1, char const *set);
 int		ft_strcmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
-char    *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
