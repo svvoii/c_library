d866e0d5cd11dcd4b86ec1c6703c333107bb8b65
Sergiu BOCANCIA <sbocanci@e1r5p13.clusters.42paris.fr>
2023-05-23 01:34:26 +0200

    update

--- ft_isspace.c
+++ ft_isspace.c
@@ -6,7 +6,7 @@
 /*   By: sbocanci <sbocanci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:29:45 by sbocanci          #+#    #+#             */
-/*   Updated: 2023/03/04 16:33:15 by sbocanci         ###   ########.fr       */
+/*   Updated: 2023/05/23 01:32:18 by sbocanci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	ft_isspace(int c)
 {
-	return (str == 0x20 || (str >= 0x09 && str <= 0x0d));
+	return (c == 0x20 || (c >= 0x09 && c <= 0x0d));
 }
